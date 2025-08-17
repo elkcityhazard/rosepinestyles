f6656b826db838dbe1af2afbd4a28b3582c194be
Andrew M McCall <andrew.m.mccall@gmail.com>
2025-08-17 19:20:47 -0400

    versions

--- interfaces/types/evaluateChildNodeFunction.ts
+++ interfaces/types/evaluateChildNodeFunction.ts
@@ -1,3 +1,3 @@
-import { NodeList } from "jsr:@b-fuze/deno-dom";
+import { NodeList } from "jsr:@b-fuze/deno-dom@0.1.56";
 
 export type EvalChildNodeFunction = (childNodes:NodeList, out:string[]) => string[]
